91f5a4c27c348afc48cf4284454c4b5dec216021
turpenar <58417823+turpenar@users.noreply.github.com>
2021-07-19 12:43:49 -0700

    remove blueprint static path

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,8 +1,6 @@
 from flask import Blueprint
 
-main = Blueprint('main', __name__,
-                 template_folder='templates',
-                 static_url_path='main/static')
+main = Blueprint('main', __name__)
 
 from app.main import routes
 

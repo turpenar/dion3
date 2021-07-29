2b8d5e18aa0b16fbf96ab3e7396b657a777e4591
turpenar <58417823+turpenar@users.noreply.github.com>
2021-07-28 18:39:05 -0700

    database configure

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ class Config(object):
 class ProductionConfig(Config):
     DATABASE_URL = os.environ.get('DATABASE_URL_PRODUCTION')
     SQLALCHEMY_DATABASE_URI = DATABASE_URL
-    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require', port='5432')
 
 class DevelopmentConfig(Config):
     ENV = 'development'

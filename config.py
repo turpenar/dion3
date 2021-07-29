6b32b4cd019d1e18317673cda78e539a0f090923
turpenar <58417823+turpenar@users.noreply.github.com>
2021-07-28 19:01:50 -0700

    database config

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ class Config(object):
 class ProductionConfig(Config):
     DATABASE_URL = os.environ.get('DATABASE_URL_PRODUCTION')
     SQLALCHEMY_DATABASE_URI = DATABASE_URL
-    conn = psycopg2.connect(DATABASE_URL, 
+    conn = psycopg2.connect(DATABASE_URL,
                             sslmode="require")
 
 class DevelopmentConfig(Config):
